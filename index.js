f8afc2e7126ee6ef021dd438246a0771f2416609
sanyaswb <sanyaswb@gmail.com>
2022-10-31 19:58:57 +0200

    recompleted 04_price-of-products

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function priceOfProducts(saltBeefPrice, saltBeefRequest, riceRequest) {
 	const ricePrice = Math.round(saltBeefPrice / 3 * 100) / 100 * riceRequest;
-	const totalSaltBeefPrice = saltBeefPrice  * saltBeefRequest;
-	const totalProductsPrice = (ricePrice + totalSaltBeefPrice).toFixed(2);
-	return +totalProductsPrice;
+	const totalSaltBeefPrice = saltBeefPrice * saltBeefRequest;
+	const totalProductsPrice = ricePrice + totalSaltBeefPrice;
+	return totalProductsPrice;
 }
 
 module.exports = priceOfProducts;
